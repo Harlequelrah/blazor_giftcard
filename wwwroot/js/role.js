e3366d3b48a0a2be70bf5269b7b29809c46bee8b
Harlequelrah <degbovimax19@gmail.com>
2024-08-25 23:26:23 +0000

    style pour que les champs de type password puissent afficher et retrait du mecanisme empechant le rechargement des pages

--- wwwroot/js/role.js
+++ wwwroot/js/role.js
@@ -1,14 +1,13 @@
-// wwwroot/js/scripts.js
 
 $(document).ready(function () {
-    // Fonction pour gérer la visibilité en fonction de l'argument
+
+
     window.manageVisibility = function (role) {
-        // Cache tous les éléments avec la classe 'subscriber'
         if (role === "SUBSCRIBER") {
             $(".subscriber").show();
             $(".admin").hide();
         }
-        // Cache tous les éléments avec la classe 'admin'
+
         else if (role === "ADMIN") {
             $(".subscriber").hide()
             $(".admin").show();
@@ -33,10 +32,8 @@ $(document).ready(function () {
         }
     };
     window.setSubscriber = function (NomSubscriber, BudgetSubscriber) {
-        // Met à jour le texte de l'élément avec l'ID 'NomSubscriber'
         $("#NomSubscriber").text(NomSubscriber);
 
-        // Met à jour le texte de l'élément avec l'ID 'SoldeSubscriber'
         $("#BudgetSubscriber").text(BudgetSubscriber);
     };
     window.hello = function (Nom) {
